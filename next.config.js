109e525f9e093037dcbc18be097859455d49aead
Trevor Wong <trevor8568@gmail.com>
2021-05-16 12:49:59 -0400

    finally got it to build after a lot of debugging

--- next.config.js
+++ next.config.js
@@ -1,1 +1,17 @@
-module.exports = { target: 'serverless' };
+const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
+
+module.exports = { 
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        // Note: we provide webpack above so you should not `require` it
+        // Perform customizations to webpack config
+        let plugin = new FilterWarningsPlugin({
+            exclude: [/mongodb/, /mssql/, /mysql/, /mysql2/, /oracledb/, /pg/, /pg-native/, /pg-query-stream/, /react-native-sqlite-storage/, /redis/, /sqlite3/, /sql.js/, /typeorm-aurora-data-api-driver/]
+        })
+
+        config.plugins.push(plugin);
+    
+        // Important: return the modified config
+        return config
+      },
+    target: 'serverless' 
+};
