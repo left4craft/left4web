afcfcef3891005078ae2ed44c2046927c3f53896
Trevor Wong <trevor8568@gmail.com>
2021-08-18 21:21:42 -0400

    add next.js specific linting settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
 	},
 	'extends': [
 		'eslint:recommended',
-		'plugin:react/recommended'
+		'plugin:react/recommended',
+		'plugin:@next/next/recommended'
 	],
 	'parserOptions': {
 		'ecmaFeatures': { 'jsx': true },
@@ -191,5 +192,6 @@ module.exports = {
 			'error',
 			'always'
 		]
-	}
+	},
+	'settings': { 'react': { 'version': 'detect' } }
 };
