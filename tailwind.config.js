030518580598c38b97116faa65f57006fa7880d5
Trevor Wong <trevor8568@gmail.com>
2021-09-04 14:44:02 -0500

    why does this not build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
 	darkMode: 'class', // or 'media' or 'class'
 	plugins: [],
-	purge: [],
+	purge: ['./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}'],
 	// eslint-disable-next-line no-unused-vars
 	theme: {
 
