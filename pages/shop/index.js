60d75f3ebe9897c3763e16d6c64ff404b545fbf0
Trevor Wong <trevor8568@gmail.com>
2021-10-02 18:08:35 -0500

    update package versions, fix linting errors

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -66,7 +66,9 @@ export default function Shop() {
 					</dt>
 					<dd className="mb-16">
 						<p>
-                    Running existing machine learning projects in Valohai is very simple! Integration only requires adding a valohai.yaml configuration file. Moving projects in and out of Valohai is easy – the integration is only the configuration file.
+                    Running existing machine learning projects in Valohai is very simple!
+					Integration only requires adding a valohai.yaml configuration file.
+					Moving projects in and out of Valohai is easy – the integration is only the configuration file.
 						</p>
 					</dd>
 					<dt className="mb-4">
@@ -76,7 +78,10 @@ export default function Shop() {
 					</dt>
 					<dd className="mb-16">
 						<p>
-                    We don’t. Valohai isn’t a data science platform; it&#x27;s a Machine Learning Management Platform that handles the whole ML pipeline from feature extraction, to training of your model and to deploying it into production in a reproducible manner. Data science platforms offer hosted notebooks and AutoML solutions.
+                    We don’t. Valohai isn’t a data science platform; it&#x27;s a Machine Learning Management Platform that
+					handles the whole ML pipeline from feature extraction,
+					to training of your model and to deploying it into production in a reproducible manner.
+					Data science platforms offer hosted notebooks and AutoML solutions.
 						</p>
 					</dd>
 					<dt className="mb-4">
@@ -86,7 +91,9 @@ export default function Shop() {
 					</dt>
 					<dd className="mb-16">
 						<p>
-                    Depends. Most of our customers use their own cloud and thus pay for usage according to their own agreements. Valohai doesn&#x27;t charge anything on top of the per-user license fee. If you don&#x27;t have a cloud provider, you can use our AWS, GCP and Azure accounts, and we&#x27;ll only charge you for what you use.
+                    Depends. Most of our customers use their own cloud and thus pay for usage according to their own agreements.
+					Valohai doesn&#x27;t charge anything on top of the per-user license fee. If you don&#x27;t have a cloud provider,
+					you can use our AWS, GCP and Azure accounts, and we&#x27;ll only charge you for what you use.
 						</p>
 					</dd>
 				</dl>
