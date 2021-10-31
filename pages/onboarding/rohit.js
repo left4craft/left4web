e2a872188e5e62c22b1b105394f5ffbf675e57d0
Rohit Narayanan <rohitmen01@gmail.com>
2021-10-31 14:31:33 -0500

    bet

--- pages/onboarding/rohit.js
+++ pages/onboarding/rohit.js
@@ -1,5 +1,27 @@
 // visit this page at http://localhost:3000/onboarding/trevor
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 export default function Page() {
-	return <p>Hello World!</p>;
+	const [count, setCount] = useState[1];
+
+	return <>
+		<h1>Hello</h1>
+		<CounterButton count = {count} setCount = {setCount} />
+		<CounterDisplay count = {count} />
+	</>;
+}
+
+function CounterButton(props) {
+	return <button onClick= { () => props.setCount(props.count+1) }> Count: {props.count} </button>;
+}
+
+function CounterDisplay(props) {
+	if (props.count % 15 === 0) {
+		return <p>FizzBuzz</p>;
+	} else if (props.count % 3 === 0) {
+		return <p>Fizz</p>;
+	} else if (props.count % 5 === 0) {
+		return <p>Buzz</p>;
+	}
 }
