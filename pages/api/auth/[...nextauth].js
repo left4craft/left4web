c7183d5ea60f17052e1c7833127e2be47fc94536
Trevor Wong <trevor8568@gmail.com>
2021-12-11 11:40:42 -0600

    fix next-auth, add cloud formation template for tables

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,31 @@
 import NextAuth from 'next-auth';
 import DiscordProvider from 'next-auth/providers/discord';
 
-const { DynamoDBAdapter } = require('../../../utils/nextauth_db_adapter');
+import { DynamoDB } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBAdapter } from '@next-auth/dynamodb-adapter';
+
+const config = {
+	credentials: {
+		accessKeyId: process.env.DYNAMODB_ACCESS_KEY_ID,
+		secretAccessKey: process.env.DYNAMODB_ACCESS_KEY_SECRET
+	},
+	region: process.env.DYNAMODB_REGION
+};
+
+const client = DynamoDBDocument.from(new DynamoDB(config), {
+	marshallOptions: {
+		convertClassInstanceToMap: true,
+		convertEmptyValues: true,
+		removeUndefinedValues: true
+	}
+});
 
 export default NextAuth({
-	adapter: DynamoDBAdapter(),
+	adapter: DynamoDBAdapter(
+		client,
+		{ tableName: process.env.DYNAMODB_NEXTAUTH_TABLE }
+	),
 	jwt: {
 		signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
 		verificationOptions: { algorithms: ['HS512'] }
