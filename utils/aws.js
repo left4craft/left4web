b75dc3fb385b5591080cce172d2aaf51dd1b78d2
Trevor Wong <trevor8568@gmail.com>
2021-12-11 21:37:39 -0600

    finish stripe webhook

--- utils/aws.js
+++ utils/aws.js
@@ -7,3 +7,12 @@ export const ddb = new aws.DynamoDB({
 	region: process.env.DYNAMODB_REGION,
 	secretAccessKey: process.env.DYNAMODB_ACCESS_KEY_SECRET
 });
+
+export const sqs = new aws.SQS({
+	accessKeyId: process.env.DYNAMODB_ACCESS_KEY_ID,
+	apiVersion: '2012-11-05',
+	region: process.env.DYNAMODB_REGION,
+	secretAccessKey: process.env.DYNAMODB_ACCESS_KEY_SECRET
+});
+
+// export const sqs = new aws.SQS( {apiVersion: '2012-11-05'} );
