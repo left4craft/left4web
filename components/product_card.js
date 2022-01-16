fa7343a3e08cc915dd3c13217d175a07d1e4fc3c
Isaac <git@eartharoid.me>
2022-01-16 00:49:56 +0000

    card images

--- components/product_card.js
+++ components/product_card.js
@@ -4,10 +4,18 @@ import Image from 'next/image';
 export default function ProductCard(props) {
 	return (
 		<div className="flex flex-nowrap mx-2 sm:mx-4 md:mx-6 lg:mx-6 pb-6">
+			<style>{`
+				#img-${props.slug} {
+					background-image: url("${props.image}");
+					height: 150px;
+					mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1.0) 50%, transparent 100%);
+					padding: 0px;
+				}
+			`}</style>
 			<div className="inline-block px-1">
 				<div className="p-4 min-w-max sm:min-w-min max-w-xs overflow-hidden rounded-lg shadow-sm bg-light text-center"> {/* w-72 h-96 */}
-					{/* <div className="bg-no-repeat bg-top bg-cover p-0" style="background-image: url('https://static.eartharoid.me/sharex/22/01/VIP%20Orange.png');"></div> */}
-					<Image src={props.image} width="110%" height="100%" className="bg-no-repeat bg-top bg-cover p-0"></Image>
+					<div id={`img-${props.slug}`} className="bg-no-repeat bg-top bg-cover m- op-0"></div>
+					{/* <Image src={props.image} width="110%" height="100%" className="bg-no-repeat bg-top bg-cover p-0"></Image> */}
 					<div className="p-1 text-white">
 						<h3 className="font-semibold text-2xl">{props.name}</h3>
 						<h4 className="text-lg text-gray-300">{props.price}</h4>
