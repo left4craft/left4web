1bfbbddaa078c6495b8215260113bad35804b557
Trevor Wong <trevor8568@gmail.com>
2022-11-22 19:14:59 -0500

    update packages, attempt to fix next-auth redirect issue

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -27,6 +27,21 @@ export default NextAuth({
 		client,
 		{ tableName: process.env.DYNAMODB_NEXTAUTH_TABLE }
 	),
+	// https://github.com/nextauthjs/next-auth/discussions/4445
+	callbacks: {
+		async redirect(params) {
+			const { url } = params;
+
+			// url is just a path, e.g.: /videos/pets
+			if (!url.startsWith('http')) return url;
+
+			// If we have a callback use only its relative path
+			const callbackUrl = new URL(url).searchParams.get('callbackUrl');
+			if (!callbackUrl) return url;
+
+			return new URL(callbackUrl).pathname;
+		}
+	},
 	jwt: {
 		signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
 		verificationOptions: { algorithms: ['HS512'] }
