7cf0a8947964ce9bea596c01d6e4a92d04dbd26f
Trevor Wong <16022847+CaptnSisko@users.noreply.github.com>
2026-08-22 12:53:39 -0700

    Add ahrefs script

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function Document() {
+	return (
+		<Html>
+			<Head>
+				<script src="https://analytics.ahrefs.com/analytics.js" data-key="g+nYeBlti4mpM1QOu2D6Lg" async />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
